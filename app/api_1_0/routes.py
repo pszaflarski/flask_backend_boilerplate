6714cc022dba26081832d145f5d2a8e0b4afbda9
u6074135 <peter.szaflarski@gmail.com>
2019-09-06 09:47:31 -0400

    cleaned up routes with serializers

--- app/api_1_0/routes.py
+++ app/api_1_0/routes.py
@@ -2,16 +2,16 @@ import logging
 
 from flask import request
 from flask_restplus import Resource
-from flask_restplus import fields
 
-from .restplus import api, token_required, admin_token_required
-from .serializers import user
+from .restplus import api, admin_token_required
+from .serializers import get_user, create_user, get_users, update_user, user_token
 from .. import db
 from ..models import User
 
 log = logging.getLogger(__name__)
 
 ns_user = api.namespace('user', description='Operations related to User Management')
+
 
 @ns_user.route('/')
 class UserCollection(Resource):
@@ -22,23 +22,12 @@ class UserCollection(Resource):
         """
         Returns list of Users.
         """
-
         users = User.query.all()
-        return api.marshal(
-            users,
-            api.model('List Users Model', {
-                'id': fields.Integer(description='id'),
-                'email': fields.String(description='email'),
-                'is_admin': fields.Boolean(description='user is admin')
-            }))
+        return api.marshal(users, get_users)
 
     @api.doc(security='apiKey')
     @api.response(201, 'User successfully created.')
-    @api.expect(api.model('Create User Model', {
-        'email': fields.String(required=True, description='email'),
-        'password': fields.String(required=True, description='password'),
-        'is_admin': fields.Boolean(default=False, description='user is admin')
-    }))
+    @api.expect(create_user)
     @admin_token_required
     def post(self):
         """
@@ -49,10 +38,10 @@ class UserCollection(Resource):
         is_admin = data.get('is_admin', False)
         password = data.get('password')
         user = User(email=email, password=password, is_admin=is_admin)
-
         db.session.add(user)
         db.session.commit()
-        return None, 201
+        id = user.id
+        return {"message": f"User {id} created"}, 201
 
 
 @ns_user.route('/<int:id>')
@@ -60,33 +49,29 @@ class UserCollection(Resource):
 class UserItem(Resource):
 
     @api.doc(security='apiKey')
-    @token_required
+    @admin_token_required
     def get(self, id):
         """
         Returns a User by id.
         """
         user = User.query.get(id)
-
         if not user:
             return {'message': 'user not found'}, 404
+        return api.marshal(user, get_user)
 
-        return api.marshal(
-            user,
-            api.model('List Users Model', {
-                'id': fields.Integer(description='id'),
-                'email': fields.String(description='email'),
-                'is_admin': fields.Boolean(description='user is admin')
-            }))
-
-    # @api.expect(user)
+    @api.expect(update_user)
     @api.response(204, 'User successfully updated.')
-    def put(self, id):
+    def patch(self, id):
         """
         Update a user
         """
-        # data = request.json
-        # update_user(id, data)
-        return None, 204
+        user = User.query.get(id)
+        if not user:
+            return {'message': 'user not found'}, 404
+        data = request.json
+        user.email = data.get('email', user.email)
+        user.is_admin = data.get('is_admin', user.is_admin)
+        return {"message": f"User {id} updated"}, 204
 
     @api.response(204, 'Category successfully deleted.')
     def delete(self, id):
@@ -94,19 +79,14 @@ class UserItem(Resource):
         Deletes a user.
         """
         # delete_user(id)
-        return None, 204
+        return {"message": f"User {id} deleted"}, 204
 
 
 @ns_user.route('/token')
 class TokenResource(Resource):
-    model = api.model('Login User Model', {
-        'email': fields.String(required=True, description='email'),
-        'password': fields.String(required=True, description='password'),
-        'ttl': fields.Integer(description='time token will be valid in seconds', default=300)
-    })
 
     @api.doc(security=None)
-    @api.expect(model)
+    @api.expect(user_token)
     def post(self):
         """
         Gets a token from and email and password.
@@ -115,14 +95,10 @@ class TokenResource(Resource):
         email = data.get('email')
         password = data.get('password')
         ttl = data.get('ttl', 300)
-
         user = User.query.filter_by(email=email).first()
         if not user:
             return None, 401
-
         if not user.verify_password(password):
             return None, 401
-
         token = user.get_api_token(expiration=ttl)
-
         return {'X-API-KEY': token}, 200
--- app/api_1_0/serializers.py
+++ app/api_1_0/serializers.py
@@ -1,9 +1,30 @@
 from flask_restplus import fields
 from .restplus import api
 
-user = api.model('User Model', {
-    'id': fields.Integer(readOnly=True, description='id'),
-    'email': fields.String(required=True, description='email'),
-    'is_admin': fields.Boolean(description='is_admin', default=False),
-    'password': fields.String(required=True, description='password')
-})
+get_users = api.model('Get Users Model', {
+                'id': fields.Integer(description='id'),
+                'email': fields.String(description='email'),
+                'is_admin': fields.Boolean(description='user is admin')
+            })
+
+create_user = api.model('Create User Model', {
+        'email': fields.String(required=True, description='email'),
+        'password': fields.String(required=True, description='password'),
+        'is_admin': fields.Boolean(default=False, description='user is admin')
+    })
+
+get_user = api.model('Get User Model', {
+                'id': fields.Integer(description='id'),
+                'email': fields.String(description='email'),
+                'is_admin': fields.Boolean(description='user is admin')
+            })
+update_user = api.model('Update User Model', {
+        'email': fields.String(description='email'),
+        'is_admin': fields.Boolean(description='user is admin')
+    })
+
+user_token = api.model('User Token Model', {
+        'email': fields.String(required=True, description='email'),
+        'password': fields.String(required=True, description='password'),
+        'ttl': fields.Integer(description='time token will be valid in seconds', default=300)
+    })
